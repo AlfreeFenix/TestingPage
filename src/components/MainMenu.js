cd2450901202bd30cdd54125e445a99425f2e0b3
AlfreeFenix <alfred.aracenapereira@gmail.com>
2023-09-15 15:48:59 -0400

    Implementation of MainMenu of the page

---
+++ src/components/MainMenu.js
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Link} from 'react-router-dom';
+import { useState } from 'react';
+
+function MainMenu() {
+    let currentUrl = window.location.pathname;
+    currentUrl === "/" ? currentUrl = '/calculator': currentUrl = currentUrl;
+    const itemsMenu = [
+            {id: 1, name: "Calculadora", url: "/calculator", className: currentUrl === "/calculator" ? "nav-link active": "nav-link"},
+            {id: 2, name: "Formulario", url: "/form", className: currentUrl === "/form" ? "nav-link active": "nav-link"},
+            {id: 3, name: "Tabla de registro", url: "/viewregister", className: currentUrl === "/viewregister" ? "nav-link active": "nav-link"},
+            {id: 4, name: "Tabla con submenus", url: "/viewsubmenu", className: currentUrl === "/viewsubmenu" ? "nav-link active": "nav-link"}
+        ];
+    const [items, setItems] = useState(itemsMenu);
+    const setActiveNav = (itemId) => {
+        const update = items.map(it =>
+            it.id === itemId ? {...it, className: "nav-link active"}:{...it, className: "nav-link"}
+        );
+        setItems(update);
+    };
+    return (
+        <BrowserRouter>
+            <div className ="container">
+                <header className = "d-flex justify-content-center py-3 border-bottom">
+                    <ul className ="nav nav-pills">
+                        {   
+                            items.map((item) =>{
+                                return( <li key = {item.id} ><Link className={item.className} onClick={() =>setActiveNav(item.id)} to ={item.url}>{item.name}</Link></li>)
+                            }
+                            )
+                        }
+                    </ul>
+                </header>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default MainMenu;
---
+++ src/components/PanelCalculator.js
@@ -0,0 +1,14 @@
+
+function PanelCalculator() {
+  return (
+    
+    <div className="CalPanel">
+        <input type='text'></input>
+        <input type='text'></input>
+        <button>Calcular</button>
+    </div>
+  );
+}
+
+export default PanelCalculator;
+
