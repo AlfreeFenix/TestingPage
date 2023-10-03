8d410bb733ace90630084248c7a38ccd63939126
AlfreeFenix <alfred.aracenapereira@gmail.com>
2023-10-03 09:23:03 -0400

    Added some ids in element html

--- src/components/PanelCalculator.js
+++ src/components/PanelCalculator.js
@@ -35,19 +35,19 @@ function PanelCalculator() {
       <div className="mb-3 row">
         <label className="col-sm-2 col-form-label">Numero A</label>
         <div className="col-sm-10">
-          <input type="number"  value = {numA} onChange={(e) => setNumA(e.target.value)}  className="form-control"/>
+          <input type="number"  value = {numA} onChange={(e) => setNumA(e.target.value)}  className="form-control" id="numA"/>
         </div>
       </div>
       <div className="mb-3 row">
         <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Numero B</label>
         <div className="col-sm-10">
-          <input type="number" value = {numB} onChange={(e) =>  setNumB(e.target.value)} className="form-control"/>
+          <input type="number" value = {numB} onChange={(e) =>  setNumB(e.target.value)} className="form-control" id="numB"/>
         </div>
       </div>
       <div className="mb-3 row">
         <label className="col-sm-2 col-form-label">Operacion</label>
         <div className="col-sm-10">
-          <select class="form-select form-select-sm" aria-label="Small select example" value = {operator} onChange={ (e) => (setOperator(e.target.value))} >
+          <select class="form-select form-select-sm" aria-label="Small select example" value = {operator} onChange={ (e) => (setOperator(e.target.value))} id="opt" >
             <option value="1" selected>Suma</option>
             <option value="2">Resta</option>
             <option value="3">Multiplicacion</option>
@@ -57,13 +57,13 @@ function PanelCalculator() {
       </div>
       <div className="mb-3 row">
         <div className="col-sm-2">
-          <button type="button" class="btn btn-primary" onClick={ () => operation()}>Resultado</button>
+          <button type="button" class="btn btn-primary" onClick={ () => operation()} id="btn">Resultado</button>
         </div>
       </div>
       <div className="mb-3 row">
         <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Resultado</label>
         <div className="col-sm-10">
-          <input type="number" readOnly value = {result} className="form-control"/>
+          <input type="number" readOnly value = {result} className="form-control" id="result"/>
         </div>
       </div>
     </div>
